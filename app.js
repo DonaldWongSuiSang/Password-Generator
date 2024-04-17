3833eca2eab1f54af54e837039cc1b6f12a473b7
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-04-17 13:07:03 +0800

    build: use express-handlebars as view engine and adding bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/movies')
+})
+
+app.get('/movies', (req, res) => {
+  res.render('index')
 })
 
 app.listen(port, () => {
